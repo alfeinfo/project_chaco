c193262f3b64113fb26e19738e6277c452c6f1c2
alfeinfo <alfedoinfo1485@gmail.com>
2021-08-25 12:49:20 -0300

    oden de carpetas

--- chaco_prgunt/chaco_prgunt/urls.py
+++ chaco_prgunt/chaco_prgunt/urls.py
@@ -1,10 +1,11 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.urls    import path
+from  .import views
 
-from  .views import inicio
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inicio/',inicio)
+    path('inicio/', inicio)
+   
 ]
--- chaco_prgunt/chaco_prgunt/wiews.py
+++ chaco_prgunt/chaco_prgunt/wiews.py
@@ -1,8 +1,10 @@
+
+
 from django.shortcuts import render
 
 
 def inicio(request):
 	template_name= "inicio.html"
-	ctx={ALFREDO}
+	ctx={}
 	
 	return render(request,template_name,ctx)
